f2a099accf0fdca44b4d435405f61f8627b74cfa
Michal Szmanda <michal.szmanda@student.put.poznan.pl>
2023-03-28 13:10:53 +0200

    Movie list pagination

--- assignment1/movies.php
+++ assignment1/movies.php
@@ -1,10 +1,24 @@
 <h1>Movies</h1>
 <!-- MOVIES -->
 <a href="movie_add.php">Add movie</a>
+<!-- Buttons for handling pagination -->
+<?php
+    $page = 0;
+    if (isset($_GET['page']))
+        $page = $_GET['page'];
+?>
+<div class="d-flex justify-content-left">
+    <a href="?page=0" class="btn btn-outline-secondary m-3">First</a>
+    <?php if ($page > 0) { ?>
+        <a href="?page=<?php echo $page - 1 ?>" class="btn btn-outline-secondary m-3">Previous</a>
+    <?php }?>
+    <a href="?page=<?php echo $page + 1 ?>" class="btn btn-outline-secondary m-3">Next</a>
+</div>
 <div class="d-flex flex-wrap">
 <?php
     require_once('functions.php');
-    $movies = getMovies(0, 50);
+    $pageCount = 50;
+    $movies = getMovies($page * $pageCount, $pageCount);
     foreach ($movies as $movie) {
         ?>
             <div class="card m-3">
